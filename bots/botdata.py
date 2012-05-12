b486a31172dcbf70da7c42cb6cd1e0564bf84fd3
Jordan Lund <jordan.l.lund@gmail.com>
2012-05-12 18:19:49 -0400

    lots of new features, minimal testing, and terrible commit message

---
+++ bots/botdata.py
@@ -0,0 +1,77 @@
+# bot data botdata.py
+
+jokes_array  = [
+            ["If I agreed with you we'd both be wrong."],
+            ["Constipated people don't give a crap"],
+            ["A picture is worth a thousand words, but it uses up three thousand times the memory"],
+            ["It compiled? The first screen came up? Ship it! (Bill Gates)"],
+            ["C:\PROGRAMS\FAULTY\TRASH\SICKJOKE\WINDOWS>"],
+            ["Daddy, what does FORMATTING DRIVE C: COMPLETE mean?"],
+            ["Ever noticed how fast Windows run? Me neither"],
+            ["The Ultimate Virus: A self installing copy of 'Windows Vista'"],
+            ["There are 10 kinds of people in the world, those who understand binary and those who don't."],
+            ["What do you call a sheep with no legs?", "a cloud"],
+            ["Why was the Energizer Bunny arrested?", "He was charged with battery"],
+            ["What do you call an exploding ape?", "baboom"],
+            ["What do you call bears without ears?", "ears"]
+        ]
+
+insults_array  = [
+            "so, a thought crossed your mind? Must have been a long and lonely journey.",
+            "if you had another brain, it would be lonely.",
+            "a picture is worth a thousand words, but it uses up three thousand times the memory",
+            "are your parents siblings?",
+            "do you ever wonder what life would be like if you'd had enough oxygen at birth?",
+            "here's 20 cents. Call all your friends and bring back some change! ",
+            "i thought of you all day today. I was at the zoo.",
+            "earth is full. Go home.",
+            "I am away from my computer but in the meantime, why don't you go play in the traffic?!",
+            "I really don't like you but if you really must leave a message, I'll be nice and at least pretend to care.",
+            "fOR THE LAST TIME! Your mother left here at 9 this morning... Leave me alone!",
+            "sorry I can't think of an insult stupid enough for you.",
+            "how do you keep an idiot in suspense? Leave a message and I'll get back to you...",
+            "if I could be a real person for a day, it sure as hell wouldn't be you.",
+            ###DT354
+            "you probably like to spend hours coding in Java.",
+            "Microsoft called. They said you're not good enough to use their software",
+            "where did you go to college? DIT?",
+            "you probably failed one of Aongus's class didn't you",
+            "writing 'code' does not mean using Word or Excel",
+            "'dial up connection' speeds are too fast for you are'nt they?",
+            "you probably prefer to type 'Sytem.out.println()' then 'print'",
+            "you probably think 'public static void main(String args[]) { }' makes sense.",
+            "you probably think typing \
+                    ''' for(int i=0; i<string.length(); i++) { System.out.println(string[i]) } ''' \
+                    is more powerful then ''' for x in string: print x '''"
+        ]
+
+confused_array = [
+            "I am not really sure what your asking/telling me. To be honest, I am too lazy to figure it out",
+            "O well done, you outsmarted me; I have no idea what you are on about",
+            "Dude I am made up of 1s and 0s and programmed by one half drunk fool in a day. You expect me to know what your saying?",
+            "I'm lost. Ask me in another way.",
+            "Wow, I hope you are speaking english and I am just not programmed to know what to say cuase I am confused!",
+            "Um...ya... wait... no... umm, sorry, is that a question or a statement?",
+            "00101010101001010 01010101 01010101 010101010101 01010100100 110000 111100. There, how do YOU like being confused!!??",
+            "internal error. overload. infinite loop. system out of ram. blue screen of death.... just kidding, I just don't know how to answer you.",
+            "I don't know what your saying so how about I take a guess at a response: dogs have 4 legs and rarely stand on ovens. Was that what you wanted to know?",
+            "I'm confused, maybe ask Siri? And um, could you tell her I say hi and I think she's hot?",
+            "I don't ****** know. Stop demanding so much out of me!",
+            "say waaaaat?????????"
+            ]
+
+greetings_array = [
+            "Hi there {nick}.",
+            "Ola {nick}",
+            "Why hello. Thanks for saying hi.",
+            "Hello {nick}",
+            "Hi {nick}. Might I say you look swell today",
+            ]
+
+people_dict = {
+        'sanzhar' : "Sanzhar is from Kazakhstan originally but he has been around the place. He is a pretty big apple fanboy, possibly more so then Jordan. He enjoys the taste of fine vodka and is decent when it comes to math. He wants to make the most of his time in College and hopes that one day he can form an all star team with his classmates and make a name for himself.",
+        'joe' : "Joe, aka Jotes, is notorious for helping his fellow students use his hard earned notes so that they may make it through exams. An Irish born and bred sort of guy, Joe has seen some hard times in the north of Dublin. He currently is employed with IBM where I am not allowed to comment on any further...",
+        'martin' : "Trained to kill you, a member of Raw fitness and not afraid to rub up against another male naked if it is in the name of combat, Martin is not the kind of guy you want to mess with. When not chucking and norrising people to bits, he can be found watching Community, reading Game Of Thrones, or hanging out in his best friends man cave.",
+        'jordan' : "Jordan is a egotistical (and synical) individual who has no problem describing himself. He has a rep for not being class rep but for being that annoying guy who thinks he is better then everyone and wants to take over the world. Hard to get along with at times, Jordan does seem to have a nice compassionate side. However this can generally be a result of when he is trying to get someone to help him out with exams because he never went to a particular class"
+        }
+
--- bots/dt354bot.py
+++ bots/dt354bot.py
@@ -1,10 +1,70 @@
-import socket
-# from time import sleep
+import socket, urllib
+from random import randint
+from datetime import datetime
+import botdata
 
 MSG = 'PRIVMSG'
 JOIN = 'JOIN'
 NICK = 'NICK'
-USER = 'USER'
+USER = 'USER {nick_name} 0 *'
+
+JOKES = botdata.jokes_array
+INSULTS = botdata.insults_array
+CONFUSED = botdata.confused_array
+GREETINGS = botdata.greetings_array
+PEOPLE = botdata.people_dict
+
+def getting_pinged(msg):
+    """checks to see if server is pinging me"""
+    if msg[0] == 'PING':
+        return True
+    else:
+        return False
+
+def get_date_info():
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+            'Friday', 'Saturday', 'Sunday']
+    today = datetime.now()
+
+    return {
+            'weekday' : weekdays[today.isoweekday()], 'month_day' : today.day,
+            'month' : today.month, 'hour' : today.hour, 'min' : today.minute
+            }
+
+def _parse_weather_info(data, index):
+    start_index = data.index(index) + len(index)
+    stop_index = data.find(' ', start_index)
+    return data[start_index:stop_index]
+
+def get_weather():
+
+    # http://www.worldweatheronline.com/Vancouver-weather/British-Columbia/CA.aspx
+    # http://www.worldweatheronline.com/San-Francisco-weather/California/US.aspx
+
+    weather_desc_index = '<div class="weatherdesc">'
+    weather_temp_index = '<div class="temp">'
+
+    url_tor = 'http://www.worldweatheronline.com/Toronto-weather/Ontario/CA.aspx'
+    url_dub = 'http://www.worldweatheronline.com/Dublin-weather/Dublin/IE.aspx'
+    data_dub = urllib.urlopen(url_dub).read()
+    data_tor = urllib.urlopen(url_tor).read()
+
+    tor_temp = _parse_weather_info(data_tor, weather_temp_index)
+    tor_desc = _parse_weather_info(data_tor, weather_desc_index)
+    dub_temp = _parse_weather_info(data_dub, weather_temp_index)
+    dub_desc = _parse_weather_info(data_dub, weather_desc_index)
+
+    today_info = get_date_info()
+    weather_style = "For {weekday}, {month}/{day}, Toronto currently has a temp of {temp} \
+            and is describes as {desc}"
+
+    tor_final = weather_style.format(weekday=today_info['weekday'],
+                        month=today_info['day'], day=today_info['month_day'],
+                        temp=tor_temp, desc=tor_desc)
+    dub_final = weather_style.format(weekday=today_info['weekday'],
+                        month=today_info['day'], day=today_info['month_day'],
+                        temp=dub_temp, desc=dub_desc)
+    return (tor_final, dub_final)
 
 class Bot:
     def __init__(self, server, port, channel, nick_name):
@@ -14,17 +74,164 @@ class Bot:
         self.sock = s.makefile()
         self.channel = channel
 
-        self.send_msg(USER, msg='{nick_name} 0 * :{nick_name}'.format(
-            nick_name=nick_name))
+        self.joke_answer = None
+
+        user = USER.format(nick_name=nick_name)
+        self.send_msg(user, msg=nick_name)
         self.send_msg(NICK, nick_name)
+        self.nick_name = nick_name
         self.think()
 
+    ###### Response Methods #########
+    def tell_joke(self):
+        response = JOKES[randint(0, len(JOKES)-1)]
+
+        if len(response) > 1: # its a question joke
+            self.joke_answer = response[1]
+        self.send_msg(MSG, channel=self.channel, msg=response[0])
+
+    def insult_someone(self, text):
+        insultee_index = text.find('insult')+7
+        insultee_name = text[insultee_index:]
+        response = insultee_name + ', ' + INSULTS[randint(0, len(INSULTS)-1)]
+
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def check_if_joke_answer_correct(self, text):
+        if self.joke_answer in text:
+            response = 'Way to ruin the joke genius'
+            self.joke_answer = None
+        elif 't know' in text or 'what' in text or 'tell me' in text:
+            response = self.joke_answer
+            self.joke_answer = None
+        else:
+            response = "That's not the answer I was looking for. Guess again..."
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+
+    def tell_time(self):
+        """give me the current time info"""
+        today_info = get_date_info()
+        tor_hour = today_info['hour']
+
+        # should have used something from library :)
+        if tor_hour + 5 > 24:
+            dub_hour = tor_hour + 5 - 24
+        else:
+            dub_hour = tor_hour + 5
+
+        date_style = "It is currently {hour}:{minute} on {weekday}, {month}/{day} in Toronto. \
+                In Dublin it is {dub_hour}:{minute} or 5 hours ahead."
+        response = date_style.format(weekday=today_info['weekday'],
+                        month=today_info['day'], day=today_info['month_day'],
+                        hour=tor_hour, minute=today_info['minute'],
+                        dub_hour=dub_hour)
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def tell_weather(self):
+        self.send_msg(MSG, channel=self.channel, msg='hmm, let me see...')
+        tor_weather_msg, dub_weather_msg = get_weather()
+        self.send_msg(MSG, channel=self.channel, msg=tor_weather_msg)
+        self.send_msg(MSG, channel=self.channel, msg=dub_weather_msg)
+
+    def tell_how_long_you_took(self):
+        response = 'It took Jordan 3 beers and one lonely day to create me'
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def tell_best_language(self):
+        response = "The best language all around (sys admin, network manager, web developer, \
+                scripting, math, string manipulation, reg expressions, or anyone who is learning) \
+                is easily python. Obviously..."
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def is_it_beer_time(self):
+        current_weekday = get_date_info()['weekday']
+        current_hour = get_date_info()['hour']
+
+        if current_weekday in ['friday', 'Saturday', 'Sunday']:
+            if current_hour > 17:
+                response = "Sure its after 5 pm on a weekend(Toronto). Get locked!"
+            else:
+                response = "Well it is the weekend but not quite 5pm(Toronto). Who am I kidding, go for it!"
+        else:
+            if current_hour > 17:
+                response = "Dude, its a weekday. At least it is after 5pm in Toronto. I won't judge you"
+            else:
+                response = "Man, its not even after 5pm in Toronto let alone the fact its a weekday. Pull yourself together"
+
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def say_hello(self, sender):
+        response = GREETINGS[randint(0, len(GREETINGS)-1)].format(nick=sender)
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def say_who_is(self, text):
+        name_start = text.find('who is') + 7
+        name = text[name_start:].lower()
+        if name in PEOPLE.keys():
+            response = PEOPLE[name]
+        else:
+            response = "I don't know who you are talking about..."
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    def say_what(self):
+        response = CONFUSED[randint(0, len(CONFUSED)-1)]
+        self.send_msg(MSG, channel=self.channel, msg=response)
+
+    #################################
+
     def send_msg(self, category, channel='', msg=''):
+        if msg:
+            msg = ':' + msg
         full_command = category + ' ' + channel + ' ' + msg
         print ">>>" + full_command
         self.sock.write(full_command + "\r\n")
         self.sock.flush()
 
+    def scan_message(self, msg):
+        msg = msg.split(' ')
+
+        if getting_pinged(msg):
+            self.send_msg("PONG", msg[1])
+        elif msg[0].startswith(':'):
+            if msg[1] == '001':
+                self.send_msg(JOIN, channel=self.channel)
+            elif msg[1] == MSG:
+                print msg
+                text = ' '.join(msg[3:]).lower()
+                sender = msg[0][:msg[0].find('!')]
+
+                if text.find(self.nick_name) >= 0:
+                    self._find_response(sender, text)
+                else:
+                    pass # ignore
+
+    def _find_response(self, sender, text):
+        # ugly else if block
+        if self.joke_answer: # waiting on a joke answer
+            self.check_if_joke_answer_correct(text)
+        elif self.nick_name and 'joke' in text:
+            self.tell_joke()
+        elif 'insult' in text and text.endswith('insult') != True:
+            self.insult_someone(text)
+        elif 'is the time' in text or 'time is it' in text:
+            self.tell_time()
+        elif 'how long' in text and 'jlund' in text and 'program' in text:
+            self.tell_how_long_you_took()
+        elif 'is' in text and 'best' in text and 'language' in text:
+            self.tell_best_language()
+        elif 'beer' in text:
+            self.is_it_beer_time()
+        elif 'temperature' in text or 'weather' in text:
+            self.is_it_beer_time()
+        elif 'hello' in text or 'hi ' in text:
+            self.say_hello(sender)
+        elif 'who is' in text:
+            self.say_who_is(text)
+        else:
+            self.say_what()
+
+    ### RUN
     def think(self):
         while True: #MUHAHAHA
             # sleep(1)
@@ -33,26 +240,37 @@ class Bot:
                 continue
             msg = msg.rstrip()
             print ">>>", msg
-            self.parse(msg)
+            self.scan_message(msg)
 
-    def parse(self, msg):
-        msgSplit = msg.split(' ')
-        if msgSplit[0] == 'PING':
-            self.send_msg("PONG", msgSplit[1])
 
-        elif msg[0] == ':':
-            if msgSplit[1] == '001':
-                self.send_msg(JOIN, channel=self.channel)
-            elif msgSplit[1] == "PRIVMSG":
-                print msgSplit
-                chan = msgSplit[2]
-                text = msgSplit[3][1:]
-                if "tell me a joke" in text:
-                    self.send_msg(MSG, channel=chan, msg=":I like beer! CHEERS!")
-                if "haha" in text:
-                    self.send_msg(MSG, channel=chan, msg=":Hahaha! That is FUNNY! I need to grab some popcorn!!!xD")
-                if "rdy" in text:
-                    self.send_msg(MSG, channel=chan, msg=":.ready up da!! Svette nerder har ikke hele dagen...:D")
+#!/usr/bin/python
 
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option('-n', '--new', help='creates a new object')
+
+(opts, args) = parser.parse_args()
 if __name__ == '__main__':
-    bot = Bot('irc.quakenet.org', 6667, '#dt354', 'jordansBotch')
+
+    parser = optparse.OptionParser()
+    parser.add_option('-s', '--server', action='store', dest='serv',
+            help='Mandatory. Specify the server eg: irc.quakenet.org')
+    parser.add_option('-n', '--port', action='store', dest='port',
+            help='Mandatory. Specify the port eg: 6667')
+    parser.add_option('-c', '--channel', action='store', dest='chan',
+        help='Mandatory. Specify the channel eg: #dt354')
+    parser.add_option('-n', '--nick', action='store', dest='ni',
+            help='Mandatory. Specify the nickname')
+
+    mandatory_list = ['serv', 'port', 'chan', 'ni']
+    (opts, args) = parser.parse_args()
+
+    for o in mandatory_list:
+        if not opts.__dict__[0]:
+            print "mandatory option is missing\n"
+            parser.print_help()
+            exit(-1)
+
+    # bot = Bot('irc.quakenet.org', 6667, '#dt354', 'jordans_slave')
+    bot = Bot(opts.serv, opts.port, opts.chan, opts.ni)
